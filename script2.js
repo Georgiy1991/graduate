af993843ec49834ced223f43887996541529169f
Jora <georgiy.bulda.1991@gmail.com>
2020-06-07 20:07:45 +0300

    first commit

---
+++ script2.js
@@ -0,0 +1,93 @@
+const catalogProduct = [
+    {
+        id: 'id1',
+        name: 'Western',
+        img: './pictures/goods/guitars/guitar1.jpg',
+        price: `${650} BYN`
+    },
+    {
+        id: 'id2',
+        name: 'Cort',
+        img: './pictures/goods/guitars/guitar2.jpg',
+        price: `${320} BYN`
+    },
+    {
+        id: 'id3',
+        name: 'Lag',
+        img: './pictures/goods/guitars/guitar3.jpg',
+        price: `${875} BYN`
+    },
+    {
+        id: 'id4',
+        name: 'Accordeon',
+        img: './pictures/goods/classic/accordeon2.jpg',
+        price: `${13650} BYN`
+    },
+    {
+        id: 'id5',
+        name: 'Bayan',
+        img: './pictures/goods/classic/bayan1.jpg',
+        price: `${8512} BYN`
+    },
+    {
+        id: 'id6',
+        name: 'Violin',
+        img: './pictures/goods/classic/violin.jpg',
+        price: `${1200} BYN`
+    },
+    {
+        id: 'id7',
+        name: 'Peace',
+        img: './pictures/goods/drums/drum1.jpg',
+        price: `${340} BYN`
+    },
+    {
+        id: 'id8',
+        name: 'Sonor',
+        img: './pictures/goods/drums/drum2.jpg',
+        price: `${710} BYN`
+    },
+    {
+        id: 'id9',
+        name: 'Electric Drums',
+        img: './pictures/goods/drums/electric_drums.jpg',
+        price: `${1500} BYN`
+    },
+    {
+        id: 'id10',
+        name: 'Midi keyboard',
+        img: './pictures/goods/piano/midi.png',
+        price: `${422} BYN`
+    },
+    {
+    id: 'id11',
+    name: 'Piano',
+    img: './pictures/goods/piano/piano.jpg',
+    price: `${2569} BYN`
+    },
+    {
+    id: 'id12',
+    name: 'Sintezator',
+    img: './pictures/goods/piano/sintez.jpg',
+    price: `${650} BYN`
+    },
+    {
+    id: 'id13',
+    name: 'Flute',
+    img: './pictures/goods/pipes/flute.jpg',
+    price: `${650} BYN`
+    },
+    {
+    id: 'id14',
+    name: 'Saxofone',
+    img: './pictures/goods/pipes/sax.jpg',
+    price: `${650} BYN`
+    },
+    {
+    id: 'id15',
+    name: 'Trombon',
+    img: './pictures/goods/pipes/trombone.jpg',
+    price: `${650} BYN`
+    }
+
+    ]
